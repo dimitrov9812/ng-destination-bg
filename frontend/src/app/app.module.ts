6b0a973045a8e361286000bbf3625a2fdd3a54a0
Alexander Dimitrow <adimitrow@vmware.com>
2021-02-21 19:48:18 +0200

    Initial folder structure

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { ContentComponent } from './components/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SideNavComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
